157c3aff0bbca504e900164f835b107d11a4b272
unknown <waiyan.htet@intern2.keppelland.com>
2013-11-26 18:08:03 +0800

    organized code in module pattern

--- js/stickynote.js
+++ js/stickynote.js
@@ -1,37 +1,99 @@
-$(controller);
+$(NoteHandler);
 
-var draggableOptions={
-	containment:'body',
-	cursor:'crosshair',
-	stack:'.note-pile',
-	revert:'invalid',
-	revertDuration:500,
-	zIndex:100,
-	helper:'clone'
-}
-var droppableOptions={
-	tolerance:'touch',
-	hoverClass:'drop-over',
-	drop:function(event,ui){
-	var $newNote=ui.helper.clone(false);
-	$newNote.css({
-		position:'absolute',
-			top:0,
-			left:0
-	});
-		$newNote.removeClass('ui-draggable-dragging');
-		$(this).addClass("add-border");
-		$(this).append($newNote);
-		$(this).droppable("option","disabled",true);
-		createNoteDrop();
-		addDeleteBtn();
-		event.preventDefault();
+//defining sticky note app
+var StickyNote={}
+
+StickyNote.DragAndDrop={
+	//draggable option
+	draggableOptions:{
+		containment:'body',
+		cursor:'crosshair',
+		stack:'.note-pile',
+		revert:'invalid',
+		revertDuration:500,
+		zIndex:100,
+		helper:'clone'
 	},
-}
-function controller(){
-	createNoteDrop();
 
-	$('.note-pile').draggable(draggableOptions).dblclick(function(){
+	//droppable options
+	droppableOptions:{
+		tolerance:'touch',
+		hoverClass:'drop-over',
+		drop:function(event,ui){
+			var $newNote=ui.helper.clone(false);
+			$newNote.css({
+				position:'absolute',
+				top:0,
+				left:0
+			});
+			$newNote.removeClass('ui-draggable-dragging');
+			$(this).addClass("add-border");
+			$(this).append($newNote);
+			$(this).droppable("option","disabled",true);
+			StickyNote.createNoteDrop();
+			StickyNote.addDeleteBtn();
+			event.preventDefault();
+		}
+	}
+}
+
+//to create droppable area 
+StickyNote.createNoteDrop=function(){
+	var noteCount=$(".right-side .note-drop").length;
+	console.log(noteCount);
+
+	var $noteDrop=$("<div></div>");
+		$noteDrop.addClass("note-drop");
+		$noteDrop.droppable(this.DragAndDrop.droppableOptions);
+		$('.right-side').append($noteDrop);
+}
+
+//to attach delete to sticky notes
+StickyNote.addDeleteBtn=function(){
+	var $note=$('.note-drop div');
+	var $delBtn=$('<span>').text('x').addClass('close');
+	$delBtn.appendTo($note);
+}
+
+//to capture mouse hover event on sticky notes
+StickyNote.enterToNote=function(){
+	$(document).on("mouseenter",".note-drop div",function(event){
+		//console.log("mouse entering to note");
+		$(this).find('span.close').css("display","inline");
+	})
+}
+
+//to capture mouse leave event on sticky notes
+StickyNote.leaveFromNote=function(){
+	$(document).on("mouseleave",".note-drop div",function(event){
+		//console.log("mouse leaving from note");
+		$(this).find('span.close').css("display","none");
+	})
+}
+
+//to delete sticky notes
+StickyNote.deleteNote=function(){
+	$(document).on('click','span.close',function(){
+		//console.log('deleting');
+		$(this).parents('.note-drop').remove();
+	})
+}
+
+StickyNote.editNote=function(){
+	$(document).on("dblclick",".note-drop div",function(){
+		$(this).attr("contenteditable",true);
+		$(this).children("span").hide();
+	}).on("focusout",'.note-drop div',function(){
+		$(this).attr("contenteditable",false);
+		$(this).children("span").show();
+	})
+}
+
+
+function NoteHandler(){
+	StickyNote.createNoteDrop();
+
+	$('.note-pile').draggable(StickyNote.DragAndDrop.draggableOptions).dblclick(function(){
 		console.log('edit');
 		$(this).draggable({disabled:true});
 		$(this).attr("contenteditable",true);
@@ -41,47 +103,10 @@ function controller(){
 		$(this).attr("contenteditable",false);
 	});
 
-	$('.note-drop').droppable(droppableOptions);
+	$('.note-drop').droppable(StickyNote.DragAndDrop.droppableOptions);
 
-	enterToNote();
-	leaveFromNote();
-	deleteNote();
-}
-
-//creating a new note droppable area depending on number of
-//already created notes
-function createNoteDrop(){
-	var noteCount=$(".right-side .note-drop").length;
-	console.log(noteCount);
-
-	var $noteDrop=$("<div></div>");
-		$noteDrop.addClass("note-drop");
-		$noteDrop.droppable(droppableOptions);
-		$('.right-side').append($noteDrop);
-}
-function addDeleteBtn(){
-	var $note=$('.note-drop div');
-	var $delBtn=$('<span>').text('x').addClass('close');
-	$delBtn.appendTo($note);
-}
-
-function enterToNote(){
-	$(document).on("mouseenter",".note-drop div",function(event){
-		//console.log("mouse entering to note");
-		$(this).find('span.close').css("display","inline");
-	})
-}
-
-function leaveFromNote(){
-	$(document).on("mouseleave",".note-drop div",function(event){
-		//console.log("mouse leaving from note");
-		$(this).find('span.close').css("display","none");
-	})
-}
-
-function deleteNote(){
-	$(document).on('click','span.close',function(){
-		//console.log('deleting');
-		$(this).parents('.note-drop').remove();
-	})
+	StickyNote.enterToNote();
+	StickyNote.leaveFromNote();
+	StickyNote.deleteNote();
+	StickyNote.editNote();
 }
